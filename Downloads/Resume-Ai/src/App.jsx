3246b801a40315bdce7b5ef7c2ba522525c89705
RohitKumar7830 <rohitbr783@gmail.com>
2024-07-17 01:20:19 -0400

    add

---
+++ Downloads/Resume-Ai/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Content from './components/Content'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <div>
+        <Content/>
+      </div>
+    </div>
+  )
+}
+
+export default App
